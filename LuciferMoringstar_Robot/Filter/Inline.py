35730761ddf30ada73ce067534b99720de5a5e0a
Anshugood <89267046+Anshugood@users.noreply.github.com>
2021-10-09 18:32:58 +0530

    Update Inline.py

--- LuciferMoringstar_Robot/Filter/Inline.py
+++ LuciferMoringstar_Robot/Filter/Inline.py
@@ -89,8 +89,8 @@ async def answer(bot, query):
 
 def get_reply_markup(query):
     buttons = [[
-        InlineKeyboardButton('Support Group', url='t.me/Mo_Tech_Group'),
-        InlineKeyboardButton('More Botz', url='t.me/MT_Botz')
+        InlineKeyboardButton('Moviez Group', url='t.me/GorgeousAndGloreous'),
+        InlineKeyboardButton('More Botz', url='t.me/Anshu888o')
         ],[
         InlineKeyboardButton('🖥️ How To Own 🖥️', url=f'{TUTORIAL}')
         ],[
